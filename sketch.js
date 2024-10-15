0151ccad32c33b7ea671de3b2503382571292e74
Allard van der Willik <allard.vanderwillik@student.hu.nl>
2024-10-15 09:11:45 +0200

    Small edits

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+let classifier;
+let video;
+let label = "Waiting...";
+
+function preload() {
+  classifier = ml5.imageClassifier("MobileNet",
+  { flipped: true }
+  );
+}
+
+function gotResults(results) {
+  console.log(results);
+  label = results[0].label;
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1280, 720);
+  video = createCapture(VIDEO, { flipped: true });
+  video.hide();
+  classifier.classifyStart(video, gotResults);
 }
 
 function draw() {
-  background("aqua");
+  background(220);
+  image(video, 0, 0, width, height);
+
+  //draw the label
+  rectMode(CENTER);
+  fill(0);
+  rect(width / 2, height - 50, width, 50);
+  textSize(28);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  noStroke();
+  text(label, width / 2, height -50);
 }
